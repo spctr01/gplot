d47ac11180bf454254582725ecbe54188ebc8d69
spctr01 <rakshitkhpi@gmail.com>
2020-11-03 11:45:49 +0530

    v

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="gploter", 
-    version="0.1",
-    scripts=['gploter'],
+    name="gplot", 
+    version="2.0",
+    scripts=['gplot'],
     author="rakshit kumar",
     author_email="rakshitkhpi@example.com",
     description="A  python library to plot charts from google sheet",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/spctr01/gploter",
+    url="https://github.com/spctr01/gplot",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
